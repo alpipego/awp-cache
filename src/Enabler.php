a845f8fdd48cbd0e76c87a8cb5801a8daf5edb6b
Alex Goller <alpipego@protonmail.com>
2020-02-23 12:59:51 +0700

    improve html minifying

--- src/Enabler.php
+++ src/Enabler.php
@@ -5,12 +5,13 @@
  * Date: 03.04.18
  * Time: 14:18
  */
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace Alpipego\AWP\Cache;
 
 use Alpipego\AWP\Cache\Exceptions\InvalidOptionTypeException;
 use Alpipego\AWP\Cache\Exceptions\NotCacheableRequestException;
+use voku\helper\HtmlMin;
 
 class Enabler
 {
@@ -40,9 +41,9 @@ class Enabler
 
     private function setUrl()
     {
-        $path       = '/'.trim($_SERVER['REQUEST_URI'], '/');
+        $path       = '/' . trim($_SERVER['REQUEST_URI'], '/');
         $this->url  = preg_replace_callback('/^(.+?)([?&])purge(?:=[^\/&]+)?&?(.*?)$/', function (array $matches) {
-            $str = $matches[1].(empty($matches[3]) ? '' : $matches[2]).$matches[3];
+            $str = $matches[1] . (empty($matches[3]) ? '' : $matches[2]) . $matches[3];
             if (substr($str, -1) !== '/') {
                 $str .= '/';
             }
@@ -53,12 +54,12 @@ class Enabler
         $this->doc  = array_pop($path) ?: '_';
         $path       = implode('_', $path);
         $path       = preg_replace('/[{}()\/\\@:]/', '_', $path);
-        $this->path = $path.(substr($path, -1) === '_' ? '' : '_');
+        $this->path = $path . (substr($path, -1) === '_' ? '' : '_');
     }
 
     public function cacheable(): bool
     {
-        if ( ! $this->decide()) {
+        if (!$this->decide()) {
             throw new NotCacheableRequestException(sprintf('Request cannot be cached: %s', $this->msg));
         }
 
@@ -73,7 +74,7 @@ class Enabler
             $this->addMessage('request from cloudflare ');
         }
         // don't cache if user is logged in
-        if (strpos('test '.implode(' ', array_keys($_COOKIE)), 'wordpress_logged_in')) {
+        if (strpos('test ' . implode(' ', array_keys($_COOKIE)), 'wordpress_logged_in')) {
             $this->cache    = false;
             $this->loggedin = true;
             $this->addMessage('loggedin user ');
@@ -92,17 +93,17 @@ class Enabler
 
         // don't cache if url has a query string
         // TODO verify that this is reasonable
-        //        if (parse_url($this->url, PHP_URL_QUERY)) {
-        //            $this->cache = false;
-        //            $this->addMessage('query string ');
-        //        }
+        if (parse_url($this->url, PHP_URL_QUERY)) {
+            $this->cache = false;
+            $this->addMessage('query string ');
+        }
 
         return $this->cache;
     }
 
-    public function addMessage(string $msg)
+    public function addMessage(string $msg): self
     {
-        $this->msg .= $msg;
+        $this->msg .= $msg . "\n";
 
         return $this;
     }
@@ -113,7 +114,7 @@ class Enabler
             $req  = parse_url($value);
             $path = trim($req['path'], '/');
 
-            return '/'.$path.'/';
+            return '/' . $path . '/';
         }, apply_filters('alpipego/awp/cache/blacklist', []));
 
         return (is_404() || is_search() || in_array($this->url, $blacklist));
@@ -129,19 +130,22 @@ class Enabler
         return $this->path;
     }
 
-    public function getDoc()
+    public function getDoc(): string
     {
         return $this->doc;
     }
 
-    // time diff
     public function time(float $start, float $end): float
     {
         return round(($end - $start), 5);
     }
 
-    public function isPurge()
+    public function isPurge(): bool
     {
+        if (defined('CACHE_DEBUG') && CACHE_DEBUG) {
+            return isset($_GET['purge']);
+        }
+
         return $this->loggedin && isset($_GET['purge']);
     }
 
@@ -153,17 +157,6 @@ class Enabler
 
         require_once $wpBlogHeader;
 
-        // get contents of output buffer
-        $html = ob_get_clean();
-        $html = preg_replace('%/(?:\h*\*)+.+?(\*\h*)+/%sm', '', $html);
-        $arr  = preg_split('/\n+|\r+/', $html);
-        array_walk($arr, function (&$value) {
-            $value = preg_replace('%[\t]|\h{2,}%', '', $value);
-            if (preg_match('/(.*?)\/\/(.*?)/', $value, $matches)) {
-                $value .= "\n";
-            }
-        });
-
-        return str_replace(["\n", "\r", "\t"], '', implode(' ', array_filter($arr)));
+        return (new HtmlMin())->minify(ob_get_clean());
     }
 }
