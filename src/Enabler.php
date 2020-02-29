701f81c679acb886b4b5846c510e46455ea08f60
Alex Goller <alpipego@protonmail.com>
2020-02-29 16:40:00 +0700

    fix ini_set type, only disable errors if not debug

--- src/Enabler.php
+++ src/Enabler.php
@@ -152,9 +152,11 @@ class Enabler
     {
         ob_start();
 
-        define('WP_DEBUG_DISPLAY', false);
-        error_reporting(0);
-        ini_set('display_errors', 0);
+        if (!defined('CACHE_DEBUG') || !CACHE_DEBUG) {
+            define('WP_DEBUG_DISPLAY', false);
+            error_reporting(0);
+            ini_set('display_errors', '0');
+        }
 
         require_once $wpBlogHeader;
 
